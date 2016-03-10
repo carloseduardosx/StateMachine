eabbf6672abb496fd5da9db0c69bbcc69a694e41
Carlos Eduardo <carloseduardosx2015@gmail.com>
2016-03-09 21:14:41 -0300

    Create class to store constants and Add option to show state machine information

--- src/com/carloseduardo/state/machine/run/Runner.kt
+++ src/com/carloseduardo/state/machine/run/Runner.kt
@@ -2,6 +2,7 @@ package com.carloseduardo.state.machine.run
 
 import com.carloseduardo.state.machine.action.StateAction
 import com.carloseduardo.state.machine.constants.State
+import com.carloseduardo.state.machine.constants.Symbols
 import com.carloseduardo.state.machine.validation.StateValidation
 import com.sun.javaws.exceptions.InvalidArgumentException
 
@@ -9,13 +10,15 @@ class Runner {
 
     private var currentState = State.Q0
     private val inputProduction = "1"
-    private val leave = "2"
+    private val showStateMachineInformation = "2";
+    private val leave = "3"
     private val success = 1
 
     private fun showActions() {
 
-        println("1- Input Production")
-        println("2- Leave")
+        println("1- Input Sentence")
+        println("2- State Machine Information")
+        println("3- Leave")
 
         print("\nChoose an option: ")
         executeAction(readLine())
@@ -29,7 +32,9 @@ class Runner {
 
         when (action) {
 
-            inputProduction -> treatProduction()
+            inputProduction -> treatSentence()
+
+            showStateMachineInformation -> showAlphabetic()
 
             leave -> System.exit(success)
 
@@ -39,9 +44,18 @@ class Runner {
         showActions()
     }
 
-    private fun treatProduction() {
+    private fun showAlphabetic() {
+        println(Symbols.STATUS_LABEL)
+        println(Symbols.STATES.format(State.Q0, State.Q1, State.Q2, State.Q3))
+        println(Symbols.SYMBOLS_LABEL)
+        println(Symbols.SYMBOLS.format("0", "1", "2"))
+        println(Symbols.TRANSITIONS_LABEL)
+        println(Symbols.TRANSITIONS)
+    }
 
-        print("Input your production: ")
+    private fun treatSentence() {
+
+        print("Input your sentence: ")
         val production = readLine();
 
         production?.forEach { currentState = StateAction().treatInput(currentState, it.toString()) }
@@ -51,7 +65,7 @@ class Runner {
             println("Production is valid!\n")
         } else if (StateValidation(currentState).isInvalidState()) {
 
-            println("Please insert a valid production!\n")
+            println("Please insert a valid sentence!\n")
         } else {
 
             println("Production invalid!\n")
--- src/com/carloseduardo/state/machine/script/run.sh
+++ src/com/carloseduardo/state/machine/script/run.sh
@@ -3,6 +3,7 @@
 kotlinc ../Main.kt \
 ../action/StateAction.kt \
 ../constants/State.kt \
+../constants/Symbols.kt \
 ../run/Runner.kt \
 ../validation/StateValidation.kt \
 -include-runtime -d State.jar
